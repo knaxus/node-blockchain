34210bb08643c4a83b252ab81619bd67449f5263
Ashok Dey <ashokdey100@gmail.com>
2018-12-25 20:39:19 +0530

    --update: random port for peer servers

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 
-const PORT = process.env.port || 3000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(PORT, () => console.log(`server started at: http://localhost:${PORT}`));
+const DEFAULT_PORT = process.env.PORT || 3000;
+let PEER_PORT = null;
+
+if (process.env.GENERATE_PEER_PORT) {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+} else {
+  PEER_PORT = DEFAULT_PORT;
+}
+
+app.listen(PEER_PORT, () => console.log(`server started at: http://localhost:${PEER_PORT}`));
