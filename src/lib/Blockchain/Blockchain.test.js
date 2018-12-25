36c1ee43b656b01e8b180eede8c1524228023ea5
Ashok Dey <ashokdey100@gmail.com>
2018-12-25 11:54:48 +0530

    --update: added isValidChain() and tests

--- src/lib/Blockchain/Blockchain.test.js
+++ src/lib/Blockchain/Blockchain.test.js
@@ -1,9 +1,13 @@
 const Block = require('../Block');
 const Blockchain = require('.');
 
-describe('Blockchain', () => {
-  const blockchain = new Blockchain();
+let blockchain = new Blockchain();
 
+beforeEach(() => {
+  blockchain = new Blockchain();
+});
+
+describe('Blockchain', () => {
   it("Should contain a 'chain' Array instance", () => {
     expect(blockchain.chain instanceof Array).toBe(true);
   });
@@ -16,5 +20,37 @@ describe('Blockchain', () => {
     const newData = 'dummy data';
     blockchain.addBlock({ data: newData });
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
+  });
+});
+
+describe('isValidChain()', () => {
+  describe('When chain does not start with the Genesis Block', () => {
+    it('Should return false', () => {
+      blockchain.chain[0].data = 'random-data';
+      expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+    });
+  });
+
+  beforeEach(() => {
+    blockchain.addBlock({ data: 'Foo' });
+    blockchain.addBlock({ data: 'Bar' });
+    blockchain.addBlock({ data: 'Foo Bar' });
+    blockchain.addBlock({ data: 'Tampered' });
+  });
+
+  describe('When chain starts with Genesis Block and has multiple blocks', () => {
+    it('Should return false if a lastHash reference is changed', () => {
+      blockchain.chain[2].lastHash = 'broken-last-hash';
+      expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+    });
+
+    it('Should return false is the chain contains an invalid field', () => {
+      blockchain.chain[2].data = 'broken-last-hash';
+      expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+    });
+
+    it('Should return true if the chain does not contain any invalid block', () => {
+      expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+    });
   });
 });
--- src/lib/Blockchain/index.js
+++ src/lib/Blockchain/index.js
@@ -1,4 +1,5 @@
 const Block = require('../Block');
+const cryptoHash = require('../hash');
 
 class Blockchain {
   constructor() {
@@ -9,6 +10,34 @@ class Blockchain {
     const lastBlock = this.chain[this.chain.length - 1];
     const newBlock = Block.mineBlock({ lastBlock, data });
     this.chain.push(newBlock);
+  }
+
+  static isValidChain(chain) {
+    // check the first block of the chain is the genesis block
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
+    }
+
+    for (let i = 1; i < chain.length; i += 1) {
+      const block = chain[i];
+      const actualLastHash = chain[i - 1].hash;
+
+      const {
+        timestamp, lastHash, hash, data,
+      } = block;
+
+      // if any of the lash hash is tapered with return false
+      if (lastHash !== actualLastHash) {
+        return false;
+      }
+
+      const validateHash = cryptoHash(timestamp, lastHash, data);
+      // check if the hash of the block is correct
+      if (hash !== validateHash) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
