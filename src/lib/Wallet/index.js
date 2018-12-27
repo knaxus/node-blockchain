67d9c8b05ddab2033209e94e2d5510b52c2fd774
Ashok Dey <ashokdey100@gmail.com>
2018-12-27 22:16:51 +0530

    --fix : converted public key to hex value

--- src/lib/Wallet/index.js
+++ src/lib/Wallet/index.js
@@ -7,7 +7,7 @@ class Wallet {
   constructor() {
     this.keyPair = ec.genKeyPair();
     this.balance = STARTING_BALANCE;
-    this.publicKey = this.keyPair.getPublic();
+    this.publicKey = this.keyPair.getPublic().encode('hex');
   }
 
   sign(data) {
