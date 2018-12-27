c48e816cc1bc94b356ce70a6fda30f624b4ec8b9
Ashok Dey <ashokdey100@gmail.com>
2018-12-27 16:06:20 +0530

    --update: unique hash generation for objects

--- src/lib/hash/genHash.test.js
+++ src/lib/hash/genHash.test.js
@@ -1,7 +1,7 @@
 const cryptoHash = require('.');
 
 describe('Generate Crypto Hash', () => {
-  const hash = '2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae';
+  const hash = 'b2213295d564916f89a6a42455567c87c3f480fcd7a1c15e220f17d7169a790b';
 
   it('Should generate a SHA-256 hex', () => {
     expect(cryptoHash('foo')).toEqual(hash);
@@ -9,5 +9,15 @@ describe('Generate Crypto Hash', () => {
 
   it('Should generate same hex for same input arguments in any order', () => {
     expect(cryptoHash('one', 'two', 'three')).toEqual(cryptoHash('two', 'one', 'three'));
+  });
+
+  it('Should create unique hash when the properties of input has changed', () => {
+    const foo = {};
+    const originalHash = cryptoHash(foo);
+
+    foo.a = 'A';
+    const newHash = cryptoHash(foo);
+
+    expect(newHash).not.toEqual(originalHash);
   });
 });
--- src/lib/hash/index.js
+++ src/lib/hash/index.js
@@ -2,7 +2,12 @@ const crypto = require('crypto');
 
 const generateCryptoHash = (...inputs) => {
   const hash = crypto.createHash('sha256');
-  hash.update(inputs.sort().join(' ')); // to make the hash multiple arguments equal regardless of argument order
+  hash.update(
+    inputs
+      .map(input => JSON.stringify(input))
+      .sort()
+      .join(' '),
+  ); // to make the hash multiple arguments equal regardless of argument order
   return hash.digest('hex');
 };
 
